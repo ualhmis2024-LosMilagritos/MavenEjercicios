192b76ab91d9c4f398bd770662c397130d1f5f85
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2023-04-05 11:55:59 +0200

    delete travis build

--- sesion07Maven/test/ual/hmis/sesion06/Ejercicio2Test.java
+++ sesion07Maven/test/ual/hmis/sesion06/Ejercicio2Test.java
@@ -2,7 +2,6 @@ package ual.hmis.sesion06;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
