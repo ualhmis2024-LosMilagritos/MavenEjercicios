023c06f422e69272b1113f0fba744b770e42df06
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2023-04-05 11:55:59 +0200

    delete travis build

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # MavenEjercicios
 
-[![Build Status](https://travis-ci.org/ualhmis/mavenEjercicios.svg?branch=master)](https://travis-ci.org/ualhmis/mavenEjercicios)
 [![codecov](https://codecov.io/gh/ualhmis/mavenEjercicios/branch/master/graph/badge.svg)](https://codecov.io/gh/ualhmis/mavenEjercicios)
     
 ## Instrucciones adicionales
--- sesion07Maven/test/ual/hmis/sesion06/Ejercicio1Test.java
+++ sesion07Maven/test/ual/hmis/sesion06/Ejercicio1Test.java
@@ -2,12 +2,8 @@ package ual.hmis.sesion06;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import ual.hmis.sesion06.Ejercicio1;
 
 
 class Ejercicio1Test {
